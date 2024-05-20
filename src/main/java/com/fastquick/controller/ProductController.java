e3c1412ddd83662740732dba9342358dce0cec0f
pmk <pmkna@naver.com>
2024-05-20 16:34:51 +0900

    무야호

--- src/main/java/com/fastquick/controller/ProductController.java
+++ src/main/java/com/fastquick/controller/ProductController.java
@@ -1,15 +1,32 @@
 package com.fastquick.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fastquick.data.dto.response.ProductListResponseDTO;
+import com.fastquick.service.ProductService;
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
 
+	@Autowired
+	private ProductService productService;
+	
     @GetMapping("/productList")
-    public String productList() {
-    	return "product/productList";
+    public ModelAndView productList(String productName, Integer page, ModelAndView mav) {
+    	mav.setViewName("/product/productList");
+    	
+    	List<ProductListResponseDTO> products = this.productService.productList(productName, page);
+    	mav.addObject("products", products);
+
+    	System.out.println(products);
+    	
+    	return mav;
     }
     
     @GetMapping("/productWrite")
---
+++ src/main/java/com/fastquick/data/entity/Product.java
@@ -0,0 +1,63 @@
+package com.fastquick.data.entity;
+
+import lombok.*;
+
+import java.util.Random;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Product extends BaseEntity {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "productId")
+    private Long productId;
+
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @Column(name = "productName", nullable = false)
+    private String productName;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "import", nullable = false)
+    private Integer importAmount;
+
+    @Column(name = "export", nullable = false)
+    private Integer exportAmount;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "barcode", unique = true, nullable = false)
+    private Long barcode;
+    
+    @PrePersist
+    public void prePersist() {
+        // barcode 필드가 비어있으면 랜덤한 6자리 숫자를 생성하여 할당
+        if (barcode == null) {
+            this.barcode = generateRandomBarcode();
+        }
+    }
+
+    private Long generateRandomBarcode() {
+        // 100000부터 999999까지의 랜덤값 생성
+        return (long) (new Random().nextInt(900000) + 100000);
+    }
+}
