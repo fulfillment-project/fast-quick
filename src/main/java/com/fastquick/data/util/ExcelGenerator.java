9131f28ef7e77d13ec6b5d6a55da2cc0fe258478
KCCI104\kcci <kuhj1021@gmail.com>
2024-05-22 12:36:27 +0900

    엑셀 파일 관련 수정

--- src/main/java/com/fastquick/data/util/ExcelGenerator.java
+++ src/main/java/com/fastquick/data/util/ExcelGenerator.java
@@ -17,21 +17,28 @@ public class ExcelGenerator {
 
     public static ByteArrayInputStream generateExcel(List<Product> products) throws IOException {
         try (Workbook workbook = new XSSFWorkbook()) {
-            Sheet sheet = workbook.createSheet("Products");
+            Sheet sheet = workbook.createSheet("AShop 상품 목록 조회");
 
+            
             Row header = sheet.createRow(0);
-            header.createCell(0).setCellValue("ID");
-            header.createCell(1).setCellValue("Name");
-            header.createCell(2).setCellValue("Barcode");
-            header.createCell(3).setCellValue("Quantity");
+            header.createCell(0).setCellValue("상품코드");
+            header.createCell(1).setCellValue("상품명");
+            header.createCell(2).setCellValue("상품 바코드");
+            header.createCell(3).setCellValue("재고");
+            header.createCell(4).setCellValue("안전재고");
+            header.createCell(5).setCellValue("입고량");
+            header.createCell(6).setCellValue("출고량");
 
             int rowIdx = 1;
             for (Product product : products) {
                 Row row = sheet.createRow(rowIdx++);
                 row.createCell(0).setCellValue(product.getProductId());
                 row.createCell(1).setCellValue(product.getProductName());
-                row.createCell(2).setCellValue(product.getBarcode());
+                row.createCell(2).setCellValue(product.getBarcode());               
                 row.createCell(3).setCellValue(product.getQuantity());
+                row.createCell(4).setCellValue(product.getSafeQuantity());
+                row.createCell(5).setCellValue(product.getImportAmount());
+                row.createCell(6).setCellValue(product.getExportAmount());
             }
 
             try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
