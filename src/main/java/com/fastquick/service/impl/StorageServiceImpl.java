61d63a40389c8a5228081cc7b8bc9939e5eb50a6
pmk <pmkna@naver.com>
2024-05-22 19:37:55 +0900

    no message

--- src/main/java/com/fastquick/service/impl/StorageServiceImpl.java
+++ src/main/java/com/fastquick/service/impl/StorageServiceImpl.java
@@ -3,7 +3,9 @@ package com.fastquick.service.impl;
 import org.springframework.stereotype.Service;
 
 import com.fastquick.data.dto.request.StorageCreateRequestDTO;
+import com.fastquick.data.entity.Member;
 import com.fastquick.data.entity.StorageRetrieval;
+import com.fastquick.data.repository.MemberRepository;
 import com.fastquick.data.repository.StorageRepository;
 import com.fastquick.service.StorageService;
 
@@ -11,26 +13,31 @@ import com.fastquick.service.StorageService;
 public class StorageServiceImpl implements StorageService{
 
 	private final StorageRepository storageRepository;
+	private final MemberRepository memberRepository;
 	
-	public StorageServiceImpl(StorageRepository storageRepository) {
+	public StorageServiceImpl(StorageRepository storageRepository, MemberRepository memberRepository) {
 		this.storageRepository = storageRepository;
+		 this.memberRepository = memberRepository;
 	}
 
 	@Override
 	public Integer storageCreate(StorageCreateRequestDTO storageCreateRequestDTO) {
-		StorageRetrieval storageRetrieval = StorageRetrieval.builder()
-				.productName(storageCreateRequestDTO.getProductName())
-				.member(storageCreateRequestDTO.getMemberId())
-				.count(storageCreateRequestDTO.getCount())
-				.warehouse(storageCreateRequestDTO.getWarehouse())
-				.zipcode(storageCreateRequestDTO.getZipcode())
-				.address(storageCreateRequestDTO.getAddress())
-				.addressDetail(storageCreateRequestDTO.getAddressDetail())
-				.bigo(storageCreateRequestDTO.getBigo())
-				.shopProduct(storageCreateRequestDTO.getShopProductId())
-				.build();
-			this.storageRepository.save(storageRetrieval);
-		return storageRetrieval.getStorageId();
+		Member member = memberRepository.findById(storageCreateRequestDTO.getMemberId())
+	            .orElseThrow(() -> new IllegalArgumentException("Invalid member ID"));
+
+	    StorageRetrieval storageRetrieval = new StorageRetrieval();
+	    storageRetrieval.setProductName(storageCreateRequestDTO.getProductName());
+	    storageRetrieval.setMember(member);
+	    storageRetrieval.setCount(storageCreateRequestDTO.getCount());
+	    storageRetrieval.setWarehouse(storageCreateRequestDTO.getWarehouse());
+	    storageRetrieval.setZipcode(storageCreateRequestDTO.getZipcode());
+	    storageRetrieval.setAddress(storageCreateRequestDTO.getAddress());
+	    storageRetrieval.setAddressDetail(storageCreateRequestDTO.getAddressDetail());
+	    storageRetrieval.setBigo(storageCreateRequestDTO.getBigo());
+	    storageRetrieval.setShopProductId(storageCreateRequestDTO.getShopProductId());
+
+	    this.storageRepository.save(storageRetrieval);
+	    return storageRetrieval.getStorageId();
 	}
 
 }
