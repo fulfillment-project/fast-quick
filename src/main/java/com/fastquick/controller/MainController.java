0090dd4e2675c5d482f1f07187d7334aa7e9daad
KCCI115\gladious <nuguri5165@naver.com>
2024-05-23 11:02:39 +0900

    메인 수정

--- src/main/java/com/fastquick/controller/MainController.java
+++ src/main/java/com/fastquick/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.fastquick.controller;
 
+import com.fastquick.data.dto.response.CountResponseDTO;
 import com.fastquick.data.dto.response.ProductListResponseDTO;
 import com.fastquick.data.dto.response.StorageListResponseDTO;
 import com.fastquick.service.impl.ProductServiceImpl;
@@ -26,9 +27,6 @@ public class MainController {
         List<StorageListResponseDTO> importList = this.storageService.importStorageList();
         List<StorageListResponseDTO> exportList = this.storageService.exportStorageList();
         List<ProductListResponseDTO> productList = this.productService.findSafeProducts();
-        System.out.println("1=======> " + importList.size());
-        System.out.println("2=======> " + exportList.size());
-        System.out.println("3=======> " + productList.size());
         ModelAndView mav = new ModelAndView();
         mav.setViewName("index");
         mav.addObject("importList", importList);
