492409b467a4680b7307595621c92da5b1969f0f
chul5 <och7164@gmail.com>
2024-05-24 01:40:03 +0900

    feat: Time Trace AOP구현

--- src/main/java/com/fastquick/aop/TimeAop.java
+++ src/main/java/com/fastquick/aop/TimeAop.java
@@ -1,2 +1,26 @@
-package com.fastquick.aop;public class TimeAop {
+package com.fastquick.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeAop {
+	@Around("execution(* com.fastquick..*(..))")
+	public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+		long start = System.currentTimeMillis();
+		System.out.println("Start:" + joinPoint.toString());
+
+		try{
+			return joinPoint.proceed();
+		}
+		finally {
+			long finish = System.currentTimeMillis();
+			long timeMs = finish - start;
+
+			System.out.println("End : " + joinPoint.toString() + " " + timeMs);
+		}
+	}
 }
